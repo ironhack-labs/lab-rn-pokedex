92b4fe430f1f7e813a508c45202365792631cad7
Eric Mora <eric.mora@digitalfemsa.com>
2023-07-28 16:22:07 -0600

    Iteration-5

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,29 @@
-import {SafeAreaView, ScrollView} from 'react-native';
-import React from 'react';
+import { FlatList, SafeAreaView, ScrollView, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { usePokeContextProvider } from '../context/usePokemon';
+import { PokeInfo } from '../interface/PokeTypes';
 
 const HomeScreen = () => {
+
+  const { pokeInfo, fetchPoke } = usePokeContextProvider()
+
+  useEffect(() => {
+    console.log('entor')
+    fetchPoke()
+  }, [])
+
   return (
     <SafeAreaView>
-      <ScrollView></ScrollView>
+      {pokeInfo && (
+        <FlatList
+          data={pokeInfo}
+          renderItem={({ item }) => <Text>{item.name}</Text>}
+          keyExtractor={(item) => item.name}
+        />
+      )}
     </SafeAreaView>
   );
 };
+
 
 export default HomeScreen;
