3ee44267e3a087571dd3547a9a278b2bf572761b
ItsDavidHdez <david-0610@live.com.mx>
2023-07-28 15:21:01 -0600

    feat: options tab navigation screen

---
+++ src/navigation/Navigation.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import HomeScreen from '../screens/HomeScreen';
+import AddPokemonScreen from '../screens/AddPokemonScreen';
+import PokemonDetailsScreen from '../screens/PokemonDetailsScreen';
+
+const Tab = createBottomTabNavigator();
+
+const Navigation = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Inicio',
+          tabBarIcon: () => <Icon name="home" color="#000" size={20} />,
+        }}
+      />
+      <Tab.Screen
+        name="PokemonDetail"
+        component={AddPokemonScreen}
+        options={{
+          tabBarLabel: 'Detalles',
+          tabBarIcon: () => <Icon name="home" color="#000" size={20} />,
+        }}
+      />
+      <Tab.Screen
+        name="Agregar"
+        component={PokemonDetailsScreen}
+        options={{
+          tabBarLabel: 'Inicio',
+          tabBarIcon: () => <Icon name="home" color="#000" size={20} />,
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default Navigation;
---
+++ src/screens/AddPokemonScreen.tsx
@@ -0,0 +1,12 @@
+import {SafeAreaView, Text} from 'react-native';
+import React from 'react';
+
+const AddPokemonScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>AddPokemonScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default AddPokemonScreen;
