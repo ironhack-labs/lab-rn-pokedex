c1f17f25d770f99e32633cd0064495033cdf1d1b
ItsDavidHdez <david-0610@live.com.mx>
2023-07-28 15:36:00 -0600

    feat: conf eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  'react/no-unstable-nested-components': [
+    'off' | 'warn' | 'error',
+    {allowAsProps: true | false},
+  ],
 };
