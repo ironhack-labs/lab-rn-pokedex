b85a87469826d18029b1983f1a35c71fe166b4cb
Michelle Penna <michelle.penna.ext@digitalfemsa.com>
2023-07-28 20:39:52 -0500

    refactor(PokemonList): change card in flatlist

--- src/components/pokemonCard.tsx
+++ src/components/pokemonCard.tsx
@@ -1,28 +1,28 @@
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import { Image, Text, TouchableOpacity } from 'react-native';
+import {Image, Text, TouchableOpacity} from 'react-native';
 import styles from '../styles/card';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {RootStackParamList} from '../navigation/navigation';
 
-type PokemonCardProps = {
-  pokemon: {
-    name: string;
-    id: number;
-    image: string;
-  };
+export type PokemonCardProps = {
+  name: string;
+  id: number;
+  image: string;
 };
 
-const PokemonCard: React.FC<PokemonCardProps> = ({pokemon}) => {
-  const navigation = useNavigation();
+const PokemonCard = ({name, id, image}: PokemonCardProps) => {
+  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
 
-  const handlePokemonPress = () => {
-    //navigation.navigate('PokemonDetail', { pokemonName: pokemon.name });
+  const handlePress = () => {
+    navigation.navigate('PokemonDetail', {pokemonName: name});
   };
 
   return (
-    <TouchableOpacity style={styles.container} onPress={handlePokemonPress}>
-      <Image source={{uri: pokemon.image}} style={styles.image} />
-      <Text>#{pokemon.id}</Text>
-      <Text>{pokemon.name}</Text>
+    <TouchableOpacity style={styles.container} onPress={handlePress}>
+      <Image source={{uri: image}} style={styles.image} />
+      <Text>#{id}</Text>
+      <Text>{name}</Text>
     </TouchableOpacity>
   );
 };
